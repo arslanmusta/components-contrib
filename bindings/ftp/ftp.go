e4c9e4b6517ceb2ade0c583399e134ad49a53109
Mustafa Arslan <mustafa.arslan1992@gmail.com>
2024-04-07 16:24:32 +0300

    Delete & refactoring

--- bindings/ftp/ftp.go
+++ bindings/ftp/ftp.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2021 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package ftp
 
 import (
@@ -6,10 +19,12 @@ import (
 	"fmt"
 	"io"
 	"path/filepath"
+	"reflect"
 	"strings"
 
 	securejoin "github.com/cyphar/filepath-securejoin"
 	"github.com/dapr/components-contrib/bindings"
+	"github.com/dapr/components-contrib/metadata"
 	"github.com/dapr/kit/logger"
 	kitmd "github.com/dapr/kit/metadata"
 
@@ -30,6 +45,8 @@ func (f *Ftp) Invoke(ctx context.Context, req *bindings.InvokeRequest) (*binding
 		return f.list(ctx, req)
 	case bindings.GetOperation:
 		return f.get(ctx, req)
+	case bindings.DeleteOperation:
+		return f.delete(ctx, req)
 	default:
 		return nil, fmt.Errorf("ftp binding error. unsupported operation %s", req.Operation)
 	}
@@ -41,6 +58,7 @@ func (f *Ftp) Operations() []bindings.OperationKind {
 		bindings.CreateOperation,
 		bindings.ListOperation,
 		bindings.GetOperation,
+		bindings.DeleteOperation,
 	}
 }
 
@@ -60,9 +78,6 @@ type createResponse struct {
 type listResponse struct {
 	Directory string     `json:"directory"`
 	FileInfos []fileInfo `json:"fileInfos"`
-}
-
-type getResponse struct {
 }
 
 type fileInfo struct {
@@ -189,18 +204,18 @@ func (f *Ftp) list(_ context.Context, req *bindings.InvokeRequest) (*bindings.In
 		return nil, fmt.Errorf("ftp binding error. ftp quit error %s: %w", dir, err)
 	}
 
-	listResponse := listResponse{
+	response := listResponse{
 		Directory: dir,
 	}
 
 	for _, entry := range entries {
-		listResponse.FileInfos = append(listResponse.FileInfos, fileInfo{
+		response.FileInfos = append(response.FileInfos, fileInfo{
 			Filename: entry.Name,
 			FileType: entry.Type.String(),
 		})
 	}
 
-	jsonResponse, err := json.Marshal(listResponse)
+	jsonResponse, err := json.Marshal(response)
 	if err != nil {
 		return nil, fmt.Errorf("ftp binding error: error encoding response as JSON: %w", err)
 	}
@@ -215,8 +230,6 @@ func (f *Ftp) get(_ context.Context, req *bindings.InvokeRequest) (*bindings.Inv
 	if err != nil {
 		return nil, fmt.Errorf("ftp binding error: error merging metadata: %w", err)
 	}
-
-	// r := strings.NewReader(string(req.Data))
 
 	filename := req.Metadata["Filename"]
 	if filename == "" {
@@ -245,18 +258,67 @@ func (f *Ftp) get(_ context.Context, req *bindings.InvokeRequest) (*bindings.Inv
 
 	res, err := c.Retr(exactFilename)
 	if err != nil {
-		return nil, fmt.Errorf("ftp binding error: retrive error fpr: %s: %w", filename, err)
+		return nil, fmt.Errorf("ftp binding error: retrieve error fpr: %s: %w", filename, err)
 	}
 	defer res.Close()
 
+	err = c.Quit()
+	if err != nil {
+		return nil, fmt.Errorf("ftp binding error: quit error %w", err)
+	}
+
 	buf, err := io.ReadAll(res)
 	if err != nil {
-		return nil, fmt.Errorf("ftp binding error: retrive error fpr: %s: %w", filename, err)
+		return nil, fmt.Errorf("ftp binding error: retrieve error fpr: %s: %w", filename, err)
 	}
 
 	return &bindings.InvokeResponse{
 		Data: buf,
 	}, nil
+}
+
+func (f *Ftp) delete(_ context.Context, req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
+	metadata, err := f.metadata.mergeWithRequestMetadata(req)
+	if err != nil {
+		return nil, fmt.Errorf("ftp binding error: error merging metadata: %w", err)
+	}
+
+	filename := req.Metadata["Filename"]
+	if filename == "" {
+		return nil, fmt.Errorf("ftp binding error: filename is empty")
+	}
+
+	_, dir, exactFilename, err := getSecureDirAndFilename(f.metadata.RootPath, filename)
+	if err != nil {
+		return nil, fmt.Errorf("ftp binding error: getting directory and file name for %s %s: %w", f.metadata.RootPath, filename, err)
+	}
+
+	c, err := ftp.Dial(metadata.Server)
+	if err != nil {
+		return nil, fmt.Errorf("ftp binding error: connection error to %s: %w", metadata.Server, err)
+	}
+
+	err = c.Login(metadata.User, metadata.Password)
+	if err != nil {
+		return nil, fmt.Errorf("ftp binding error: login error with user: %s: %w", metadata.User, err)
+	}
+
+	err = c.ChangeDir(dir)
+	if err != nil {
+		return nil, fmt.Errorf("ftp binding error: directory change error for %s: %w", dir, err)
+	}
+
+	err = c.Delete(exactFilename)
+	if err != nil {
+		return nil, fmt.Errorf(("ftp binding error: file delete error for %s: %w"), exactFilename, err)
+	}
+
+	err = c.Quit()
+	if err != nil {
+		return nil, fmt.Errorf("ftp binding error: quit error %w", err)
+	}
+
+	return &bindings.InvokeResponse{}, nil
 }
 
 func (metadata ftpMetadata) mergeWithRequestMetadata(req *bindings.InvokeRequest) (ftpMetadata, error) {
@@ -291,5 +353,12 @@ func getSecureDir(rootPath string, directory string) (secureDirectory string, er
 	}
 	secureDirectory, err = securejoin.SecureJoin(rootPath, directory)
 
+	return
+}
+
+// GetComponentMetadata returns the metadata of the component.
+func (f *Ftp) GetComponentMetadata() (metadataInfo metadata.MetadataMap) {
+	metadataStruct := ftpMetadata{}
+	metadata.GetMetadataInfoFromStructType(reflect.TypeOf(metadataStruct), &metadataInfo, metadata.BindingType)
 	return
 }
